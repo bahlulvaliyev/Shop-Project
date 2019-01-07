65739da8c7c7871df74e6e34a0bffba77f953e9a
Behlul <bvaliyev96@gmail.com>
2019-01-07 09:43:43 +0400

    []

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 
 $(document).ready(()=>{
-  $(".showcase1").click((e)=>{ 
-    $(".showcase-big").attr("src","../image/product_photo/Product Img.png");
-    
+  $(".showcase1").click(()=>{ 
+    $(".showcase-big").attr("src", "../image/product_photo/81BxSLPKITL._UY695_.jpg");
+    $(".showcase-big").attr("width","145px");
+    $(".showcase-big").attr("heigh","275px");
   });
-
-      
-
+  $(".showcase2").click(()=>{ 
+    $(".showcase-big").attr("src", "../image/product_photo/813MyjadwFL._UY500_.jpg");
+    $(".showcase-big").attr("width","145px");
+    $(".showcase-big").attr("heigh","275px");
+  });
+  $(".showcase3").click(()=>{ 
+    $(".showcase-big").attr("src", "../image/product_photo/Big Product Img.png");
+    $(".showcase-big").attr("width","145px");
+    $(".showcase-big").attr("heigh","275px");
+  });
+  $(".twiceSection__dots").click(()=>{ 
+    $(".extraProductHidden").slideDown();
+  });
 });
